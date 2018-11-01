f7011f6b9fc871068a59579d2abd1ff051eccb16
Ysugashi <sugashi0713@google.com>
2018-11-01 11:40:43 +0900

    scriptTempleteの追加、テストシーンの作成

---
+++ Assets/Scripts/BaseCharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterController : ManagedUpdateBehaviour
+{
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void UpdateMe()
+    {
+    
+    }
+
+	protected override void FixedUpdateMe()
+	{
+
+	}
+}
